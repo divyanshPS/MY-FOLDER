8566cf474f3c83b02d54f4245b6b354f1794efce
divyanshPS <divyanshps2669@gmail.com>
2023-03-24 22:38:55 +0530

    unreachable code warning due to exit(0) fixedstack2.c

--- array merging/merging2.c
+++ array merging/merging2.c
@@ -2,28 +2,28 @@
 #include<conio.h>
 #define M 100
 #define N 100
-int main(){
+void main(){
     int la1[M],la2[N],la3[M+N];
     int asize , bsize , i , j=0;
-    printf("Enter the size of 1st array : ");
+    printf("\nEnter the size of 1st array : ");
     scanf("%d",&asize);
-    printf("Enter %d elements of 1st array : ",asize);
+    printf("\nEnter %d elements of 1st array : \n",asize);
     for(i=0;i<asize;i++){
         scanf("%d",&la1[i]);
         la3[j]=la1[i];
         j++;
     }
-    printf("Enter the size of 2nd array : ");
+    printf("\nEnter the size of 2nd array : ");
     scanf("%d",&bsize);
-    printf("Enter %d elements of 2nd array : ",bsize);
+    printf("\nEnter %d elements of 2nd array : \n",bsize);
     for(i=0;i<bsize;i++){
         scanf("%d",&la2[i]);
         la3[j]=la2[i];
         j++;
     }
-    printf("Elements of merged array are : ");
+    printf("\nElements of merged array are : \n");
     for(i=0;i<asize+bsize;i++){
         printf("%d ",la3[i]);
     }
-    return 0;
+    getch();
 }
